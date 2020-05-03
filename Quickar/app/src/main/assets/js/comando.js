3bf1fb8743cd0d6e9dcb61799f760eba5e9c9e7e
Luis Enrique <luisechvz@gmail.com>
2020-05-03 04:05:36 -0500

    Novedades de quickar en sesion de soluciones

--- Quickar/app/src/main/assets/js/comando.js
+++ Quickar/app/src/main/assets/js/comando.js
@@ -1,93 +1,113 @@
-$(document).ready(function()   {
-       $('#cargador').fadeOut(4000);
-       $('.parallax').parallax();
+$(document).ready(function () {
+    $('#cargador').fadeOut(4000);
+    $('.parallax').parallax();
     //trigger menu
 
-        $(".dropdown-button").dropdown();
-        $('.tooltipped').tooltip({delay: 1});
+    $(".dropdown-button").dropdown();
+    $('.tooltipped').tooltip({ delay: 1 });
 
-         
-        $('.carousel').carousel({
-            padding: 0,
-            dist: -500,
-            shift: 10
-        });
-        
+
+    $('.carousel').carousel({
+        padding: 0,
+        dist: -500,
+        shift: 10
+    });
+
+    $('.tabs').tabs();
+
+
+
+});
+
+//Los tabs nos envian a otras paginas
+$(document).ready(function () {
+    var tabsPuchables = document.getElementsByClassName("tabsPuchables");
+    $(tabsPuchables[0]).click(function () {
+        window.open("soluciones.html", "_self");
+    });
+    $(tabsPuchables[1]).click(function () {
+        window.open("temas-seleccion.html", "_self");
+    });
+    $(tabsPuchables[2]).click(function () {
+        window.open("obd_genericos.html", "_self");
+    });
+    $(tabsPuchables[3]).click(function () {
+        window.open("piezas-seleccion.html", "_self");
+    });
+});
+
+$(document).ready(function () {
+
+    var alto = $(window).height();
+    $('#fullHeight').css({ 'height': alto });
+
+    /*efectos de carousel para la version de servicio PRO*/
+    $('.carousel.carousel-slider').carousel({ fullWidth: true });
+
+
+
+    var consulta = $(".search-table").DataTable();
+
+    $(".input-search").keyup(function () {
+        consulta.search($(this).val()).draw();
+
+
+        if ($(".input-search").val() == "") {
+            $(".search-table").hide();
+        } else {
+            $(".search-table").fadeIn();
+        };
+
+    });
+
+
+
+    var sectionMenu = document.getElementById('sectionMenu');
+    var headroom = new Headroom(sectionMenu);
+    headroom.init();
+
+
+
+    /*En esta parte se encuentran algunas llamadas
+    para poder ejecutar algunos efectos BASICOS*/
+    $('.carousel.carousel_shower').carousel({ 'dist': '100' });
+    $('.carousel.carousel-slider').carousel({ fullWidth: true });
+    $('.carousel.carousel-slider').carousel({ 'dist': '0' });
+
+
+    $('.slider').slider();
+
+    $('ul.tabs').tabs();
+    $('#tabs-swipe-demo').tabs({ 'swipeable': true });
+
+    $(".button-collapse").sideNav({});
+    $('.materialboxed').materialbox();
+
+    $('input#input_text').characterCounter();
+
+    $('.modal').modal();
+
+
+    //SLIDER DE PRODUCTOS
+    $('.carru').slick({
+        dots: false,
+        infinite: false,
+        speed: 400,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true,
+        adaptiveHeight: true,
+        swipeable: true,
+        slidesToScroll: true,
+        nextArrow: '',
+        prevArrow: ''
+    });
+
 
 });
 
 
-$(document).ready(function(){
-
-var alto = $(window).height();
-  $('#fullHeight').css({'height' : alto});
-
-  /*efectos de carousel para la version de servicio PRO*/
-$('.carousel.carousel-slider').carousel({fullWidth: true});
-
-
-
-var consulta = $(".search-table").DataTable();
-
-$(".input-search").keyup(function(){
-    consulta.search($(this).val()).draw();
-
-
-if ($(".input-search").val() == ""){
-    $(".search-table").hide();
-}else{
-    $(".search-table").fadeIn();
-};
-
-});
-
-
-
-var sectionMenu = document.getElementById('sectionMenu');
-var headroom = new Headroom(sectionMenu);
-headroom.init();
-
-
-
-/*En esta parte se encuentran algunas llamadas
-para poder ejecutar algunos efectos BASICOS*/
-$('.carousel.carousel_shower').carousel({'dist' : '100'});
-$('.carousel.carousel-slider').carousel({fullWidth: true});
-$('.carousel.carousel-slider').carousel({'dist' : '0'});
-
-
-$('.slider').slider();
-
-   $('ul.tabs').tabs();
-   $('#tabs-swipe-demo').tabs({ 'swipeable': true });
-
-$(".button-collapse").sideNav({});
- $('.materialboxed').materialbox();
-
-$('input#input_text').characterCounter();
-
-$('.modal').modal();
-
-
-//SLIDER DE PRODUCTOS
-$('.carru').slick({
-  dots: false,
-  infinite: false,
-  speed: 400,
-  slidesToShow: 1,
-  centerMode: true,
-  variableWidth: true,
-  adaptiveHeight: true,
-  swipeable: true,
-  slidesToScroll: true,
-  nextArrow: '',
-  prevArrow: ''
-  });
-
-});
-
-
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     $('.preloader').delay(10000).fadeOut('fast');
 
     $('.preloader')
